4e2992d55070ee6c5ddcc11fea4cc6a8083ed253
PedroD222 <pedroduarte19@sapo.pt>
2014-10-01 13:47:15 +0100

    proj vs

---
+++ ficha1/ficha1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ficha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("http://thoth.cc.e.ipl.pt");
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("resource/{id}", Method.POST);
+            request.AddParameter("name", "value"); // adds to POST or URL querystring based on Method
+            request.AddUrlSegment("id", 123); // replaces matching token in request.Resource
+
+            // easily add HTTP Headers
+            request.AddHeader("header", "value");
+
+            // add files to upload (works with compatible verbs)
+            request.AddFile(path);
+
+            // execute the request
+            RestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+
+            // or automatically deserialize result
+            // return content type is sniffed but can be explicitly set via RestClient.AddHandler();
+            RestResponse<Person> response2 = client.Execute<Person>(request);
+            var name = response2.Data.Name;
+
+            // easy async support
+            client.ExecuteAsync(request, response =>
+            {
+                Console.WriteLine(response.Content);
+            });
+
+            // async with deserialization
+            var asyncHandle = client.ExecuteAsync<Person>(request, response =>
+            {
+                Console.WriteLine(response.Data.Name);
+            });
+
+            // abort the request on demand
+            asyncHandle.Abort();
+        }
+    }
+}

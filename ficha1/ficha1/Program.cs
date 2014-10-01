327144995c8f9659e1bee4bfeaae45cca7a65bf6
36864 <serejo.miguel@gmail.com>
2014-10-01 14:39:57 +0100

    Added repo.cs, first pass

--- ficha1/ficha1/Program.cs
+++ ficha1/ficha1/Program.cs
@@ -9,29 +9,21 @@ namespace ficha1
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var client = new RestClient("http://thoth.cc.e.ipl.pt");
+            var client = new RestClient("http://api.github.com");
             // client.Authenticator = new HttpBasicAuthenticator(username, password);
 
-            var request = new RestRequest("resource/{id}", Method.POST);
-            request.AddParameter("name", "value"); // adds to POST or URL querystring based on Method
-            request.AddUrlSegment("id", 123); // replaces matching token in request.Resource
-
+            var request = new RestRequest("orgs/" + args[0]+ "/repos", Method.GET);
             // easily add HTTP Headers
-            request.AddHeader("header", "value");
+            request.AddHeader("Host", "api.github.com");
 
-            // add files to upload (works with compatible verbs)
-            request.AddFile(path);
-
-            // execute the request
-            RestResponse response = client.Execute(request);
-            var content = response.Content; // raw content as string
-
+            RestResponse response3 = (RestResponse) client.Execute(request);
+            Console.WriteLine(response3.Content);
             // or automatically deserialize result
             // return content type is sniffed but can be explicitly set via RestClient.AddHandler();
-            RestResponse<Person> response2 = client.Execute<Person>(request);
-            var name = response2.Data.Name;
+            RestResponse<Repo> response2 = (RestResponse<Repo>) client.Execute<Repo>(request);
+            //var name = response2.Data.Name;
 
             // easy async support
             client.ExecuteAsync(request, response =>
@@ -40,13 +32,14 @@ namespace ficha1
             });
 
             // async with deserialization
-            var asyncHandle = client.ExecuteAsync<Person>(request, response =>
-            {
-                Console.WriteLine(response.Data.Name);
-            });
+            //var asyncHandle = client.ExecuteAsync<Person>(request, response =>
+            //{
+                //Console.WriteLine(response.Data.Name);
+            //});
 
             // abort the request on demand
-            asyncHandle.Abort();
+            //asyncHandle.Abort();
+            return 0;
         }
     }
 }

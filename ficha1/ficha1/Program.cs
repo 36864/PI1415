2bb56b8280f81b12dc89699e633a8705a1ecb2d2
36864 <serejo.miguel@gmail.com>
2014-10-02 20:49:17 +0100

    Complete repo language count
    includes pagination.

--- ficha1/ficha1/Program.cs
+++ ficha1/ficha1/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using RestSharp;
-
+using RestSharp.Deserializers;
 namespace ficha1
 {
     // c419d1442700aa09acec38622f6dbceca73b74af 
+    //6cc637fc4c44405fab41ea4f837cef12d5bb9996
     class Program
     {
+
+        static void addRepos (List<Repo> list, IRestResponse source, IDeserializer deserializer){
+            list.AddRange(deserializer.Deserialize<List<Repo>>(source));
+        }
+
         static int Main(string[] args)
         {
 
@@ -19,25 +25,70 @@ namespace ficha1
                 return 1;
             }
 
-            var client = new RestClient("http://api.github.com/");
+            var client = new RestClient("https://api.github.com/");
             //client.Authenticator = new HttpBasicAuthenticator(username, password);
             
-            var request = new RestRequest("orgs/" + args[0]+ "/repos", Method.GET);
+            var request = new RestRequest("orgs/" + args[0], Method.GET);
             // easily add HTTP Headers
-            request.AddHeader("Host", "api.github.com");
-            
-            RestResponse response3 = (RestResponse) client.Execute(request);
-            Console.WriteLine(response3.Content);
-            // or automatically deserialize result
-            // return content type is sniffed but can be explicitly set via RestClient.AddHandler();
-            RestResponse<Repo> response2 = (RestResponse<Repo>) client.Execute<Repo>(request);
-            //var name = response2.Data.Name;
+            request.AddHeader("host", "api.github.com");
+            request.AddHeader("User-Agent", "36864");
 
-            // easy async support
-            client.ExecuteAsync(request, response =>
+            Org org = new Org();
+            bool lastRepo = false;
+            RestSharp.Deserializers.JsonDeserializer jsdes = new RestSharp.Deserializers.JsonDeserializer();
+            
+            client.ExecuteAsync<Org>(request, orgResponse =>
+                {
+                    org = jsdes.Deserialize<Org>(orgResponse);
+                });
+            request.Resource += "/repos";
+            IRestResponse response = client.Execute(request);
+            Parameter linkheader = response.Headers.First(a => a.Name == "Link");
+            addRepos(org.Repos, response, jsdes);
+            if (linkheader != null)
             {
-                Console.WriteLine(response.Content);
-            });
+                string linkval = linkheader.Value.ToString();
+                while (linkval.Contains("next"))
+                {
+                    string nextURI = linkval.Substring(linkval.IndexOf('?'),linkval.Length - linkval.IndexOf('>'));
+                    request.Resource = "orgs/" + args[0] + "/repos" + nextURI;
+                    response = client.Execute(request);
+                    addRepos(org.Repos, response, jsdes);
+                    linkheader = response.Headers.First(a => a.Name == "Link");
+                    linkval = linkheader.Value.ToString();
+                }
+                lastRepo = true;
+            }
+            else
+            {
+                lastRepo = true;
+            }
+            Dictionary<string, int> languages = new Dictionary<string,int>();
+            foreach (Repo repo in org.Repos)
+            {
+                if (repo.Language == null)
+                    repo.Language = "";
+                if (!languages.ContainsKey(repo.Language))
+                {
+                    languages.Add(repo.Language, 1);
+                }
+                else
+                {
+                    ++languages[repo.Language];
+                }
+                    
+            }
+            while (org.Name == null || !lastRepo) ;
+            Console.WriteLine(org.Name + "(" + org.Location + ")");
+            foreach (var item in languages)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+            // easy async support
+       //     client.ExecuteAsync(request, response =>
+            //{
+                //Console.WriteLine(response.Content);
+            //});
 
             // async with deserialization
             //var asyncHandle = client.ExecuteAsync<Person>(request, response =>
@@ -47,6 +98,7 @@ namespace ficha1
 
             // abort the request on demand
             //asyncHandle.Abort();
+            Console.Read();
             return 0;
         }
     }

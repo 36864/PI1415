447104bfb4585530836e13aa23443d76d3b6a1a1
PedroD222 <pedroduarte19@sapo.pt>
2014-12-21 20:14:51 +0000

    not reload

--- ficha3/not/not.js
+++ ficha3/not/not.js
@@ -17,27 +17,26 @@ function isOdd(v)
 	return v%2 == 1;
 }
 
-function isShit(v1, v2)
+function isMzeroMnine(v1, v2)
 {
-	return v1>1 && v2<9;
+	return v1>0 && v2<9;
 }
 
-//var isEvenNOdd = isOdd.not();
-//isEven(0, 3);
-
 var isNEven = isEven.not();
+var isNOdd = isOdd.not();
+var isMoreZMinorN = isMzeroMnine.not();
+/*
 console.log(isNEven(4));		//false
 console.log(isNEven(5));		//true
 
-var isNOdd = isOdd.not();
 console.log(isNOdd(4));			//true
 console.log(isNOdd(5));			//false
-
-$mu.test('Function Not - A', function (){
-							//var test1 = isNEven(4);
-							$mu.assert(isNEven(4) == false, "4 is ODD ?");
-							$mu.assert(isNEven(5) == true, "5 is ODD ?");
-							$mu.assert(isNOdd(4) == true, "4 is ODD ?");
-							$mu.assert(isNOdd(5) == false, "5 is ODD ?");
-	
+*/
+$mu.test('Function Not - B', function (){
+							$mu.assert(isNEven(4) == false, "isNEven(4)");
+							$mu.assert(isNEven(5) == true, "isNEven(5)");
+							$mu.assert(isNOdd(4) == true, "isNOdd(4)");
+							$mu.assert(isNOdd(5) == false, "isNOdd(5)");
+							$mu.assert(isMoreZMinorN(1, 8) == false, "isMoreZMinorN(1, 8)");
+							$mu.assert(isMoreZMinorN(0, 8) == true, "isMoreZMinorN(0, 8)");
 							});
--- ficha3/not/not_a.js
+++ ficha3/not/not_a.js
@@ -16,20 +16,26 @@ function isOdd(v)
 	return v%2 == 1;
 }
 
-function isShit(v1, v2)
+function isMzeroMnine(v1, v2)
 {
-	return v1>1 && v2<9;
+	return v1>0 && v2<9;
 }
 
 var isEvenNOdd = not(isOdd);
 var isNOdd = not(isEven);
+var isMoreZMinorN = not(isMzeroMnine);
 
-console.log(isOdd(4));		//false
+/*console.log(isOdd(4));		//false
 console.log(isEvenNOdd(4));	//true
 
 console.log(isEven(4));		//true
-console.log(isNOdd(4));		//false
+console.log(isNOdd(4));		//false*/
 
-console.log(isShit(4, 8));	//false
-var isNShit = not(isShit);
-console.log(isNShit(4, 8));	//true
+$mu.test('Function Not - A', function (){
+							$mu.assert(isEvenNOdd(4) == true, "isEvenNOdd(4)");
+							$mu.assert(isEvenNOdd(5) == false, "isEvenNOdd(5)");
+							$mu.assert(isNOdd(4) == false, "isNOdd(4)");
+							$mu.assert(isNOdd(5) == true, "isNOdd(5)");
+							$mu.assert(isMoreZMinorN(5, 4) == false, "isMoreZMinorN(5, 4)");
+							$mu.assert(isMoreZMinorN(5, 9) == true, "isMoreZMinorN(5, 9)");
+							});

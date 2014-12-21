03f2b7803af3d53693f59699f8b33cccb1373477
PedroD222 <pedroduarte19@sapo.pt>
2014-12-21 19:41:34 +0000

    add final code

--- ficha3/not/not.js
+++ ficha3/not/not.js
@@ -17,26 +17,26 @@ function isOdd(v)
 	return v%2 == 1;
 }
 
-function isShit(v1, v2)
+function isMzeroMnine(v1, v2)
 {
-	return v1>1 && v2<9;
+	return v1>0 && v2<9;
 }
 
-//var isEvenNOdd = isOdd.not();
-//isEven(0, 3);
-
 var isNEven = isEven.not();
+var isNOdd = isOdd.not();
+var isMoreZMinorN = isMzeroMnine.not();
+/*
 console.log(isNEven(4));		//false
 console.log(isNEven(5));		//true
 
-var isNOdd = isOdd.not();
 console.log(isNOdd(4));			//true
 console.log(isNOdd(5));			//false
-
+*/
 $mu.test('Function Not - B', function (){
 							$mu.assert(isNEven(4) == false, "isNEven(4)");
 							$mu.assert(isNEven(5) == true, "isNEven(5)");
 							$mu.assert(isNOdd(4) == true, "isNOdd(4)");
 							$mu.assert(isNOdd(5) == false, "isNOdd(5)");
-
+							$mu.assert(isMoreZMinorN(1, 8) == false, "isMoreZMinorN(1, 8)");
+							$mu.assert(isMoreZMinorN(0, 8) == true, "isMoreZMinorN(0, 8)");
 							});

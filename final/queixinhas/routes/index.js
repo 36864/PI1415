3d237243aa0fa46edd6bd9d4bfa4848b72d9f231
36864 <serejo.miguel@gmail.com>
2015-02-07 16:53:03 +0000

    added index redirect for authed users

--- final/queixinhas/routes/index.js
+++ final/queixinhas/routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var db = requier('dbaccess');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	if(req.user) return res.redirect('/queixinhas');
 	//check user authentication
 	//get list of top/most recent entries for unauthenticated users
 	//else get list of entries, add paging
-	res.render('index', { title: 'Express' });
+	db.getQueixinhas(1, function(err, list){
+		if(err) res.render('index');
+		res.render('index', { queixas: list });
+	});
+	
 });
 
 module.exports = router;

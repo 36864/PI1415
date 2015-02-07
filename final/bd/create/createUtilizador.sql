7e9dbd160270025029e6ffb0915ba38b1c88589a
PedroD222 <pedroduarte19@sapo.pt>
2015-02-07 19:14:08 +0000

    change create

--- final/bd/create/createUtilizador.sql
+++ final/bd/create/createUtilizador.sql
@@ -4,10 +4,11 @@
 
 CREATE TABLE utilizador
 (
-  username character(20) NOT NULL,
-  password character(20) NOT NULL,
-  email character(20) NOT NULL,
+  username character(50) NOT NULL,
+  email character(50) NOT NULL,
   "Gestor" boolean NOT NULL,
+  hash character(128) NOT NULL,
+  salt character(128) NOT NULL,
   CONSTRAINT utilizador_pkey PRIMARY KEY (username)
 )
 WITH (

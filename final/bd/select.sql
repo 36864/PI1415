d3366c57ee9d535a56304c7151be415232e60cf2
PedroD222 <pedroduarte19@sapo.pt>
2015-02-08 23:38:32 +0000

    change bd and selects

---
+++ final/bd/select.sql
@@ -0,0 +1,14 @@
+﻿SELECT id, designacao
+  FROM categoria;
+SELECT *
+  FROM categoriaqueixinha;
+ SELECT *
+  FROM comentario;
+  SELECT *
+  FROM queixinha ;
+    SELECT *
+  FROM queixinhautilizador  ;
+    SELECT *
+  FROM utilizador  ;
+    SELECT *
+  FROM votacao  ;

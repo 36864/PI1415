3e19dfb348116e46fd080532193205a7a73bbbcc
PedroD222 <pedroduarte19@sapo.pt>
2015-02-09 18:35:20 +0000

    add trg and db

--- final/bd/select.sql
+++ final/bd/select.sql
@@ -26,45 +26,8 @@ INSERT INTO categoria( designacao)
 
     delete from queixinha
 	
-CREATE FUNCTION contagemvotos() RETURNS trigger AS $contagemvotos$
-    BEGIN
-		if (NEW.correta)
-		begin
-			update queixinha set votos_corretos += 1
-			where id = NEW.id_queixinha
-		end
-		else
-		begin
-			update queixinha set votos_incorretos += 1
-			where id = NEW.id_queixinha
-		end
-        --RETURN NEW;
-    END;
-$contagemvotos$ LANGUAGE plpgsql;
 
-CREATE TRIGGER contagemvotos AFTER INSERT OR UPDATE ON votacao
-    FOR EACH ROW EXECUTE PROCEDURE contagemvotos();
 	
 	
 	
 	
-CREATE FUNCTION contvotosupnotif() RETURNS trigger AS $contvotosupnotif$
-    BEGIN
-		update queixinhautuilizador set notificacao = true, datanotificacao = now()
-		where queixinha = NEW.id_queixinha and username in (select username from queixinha where id = NEW.id_queixinha) 
-		if (NEW.correta)
-		begin
-			update queixinha set votos_corretos += 1
-			where id = NEW.id_queixinha
-		end
-		else
-		begin
-			update queixinha set votos_incorretos += 1
-			where id = NEW.id_queixinha
-		end
-        RETURN Null;
-    END;
-$contvotosupnotif$ LANGUAGE plpgsql;
-
-CREATE TRIGGER contvotosupnotif AFTER INSERT OR UPDATE OR DElete ON votacao
-    FOR EACH ROW EXECUTE PROCEDURE contvotosupnotif();
